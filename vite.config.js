aa3d2de320d77d045ac60f94bf31c3c1b1f3fcbb
khavrat <ihavrat@gmail.com>
2023-05-06 17:59:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   base: "/tweetcard",
+  build: {
+    outDir: "./build",
+    minify: true,
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+    host: "0.0.0.0",
+    hmr: true,
+  },
 });
